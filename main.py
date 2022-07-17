b615bb0f75bfb7a500865364745940beb5de089b
Manish Rawal <manish.rawal@convergetree.com>
2022-07-17 20:17:05 +0545

    front end validation with using pydantic library model

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+from typing import Optional
 from fastapi import Body, FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
 
 
 @app.get("/posts")
@@ -14,6 +23,7 @@ def root():
 
 
 @app.post("/createposts")
-def create_posts(payload: dict = Body(...)):
-    print(payload)
-    return {"message": "successfully created posts"}
+def create_posts(post: Post):
+    print(post)
+    print(post.dict())
+    return {"data": post}
