f0dec38eeabe9c193d9a0240c7a1489166d8bef6
Manish Rawal <manish.rawal@convergetree.com>
2022-07-17 12:20:04 +0545

    extract data from body of payload

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-from fastapi import FastAPI
+from fastapi import Body, FastAPI
 
 app = FastAPI()
 
+
+@app.get("/posts")
+def get_data():
+    return {"data": "welcome to posts"}
+
+
 @app.get("/")
-async def root():
-  return {"message":"Hello World"}
+def root():
+    return {"message": "Hello World"}
+
+
+@app.post("/createposts")
+def create_posts(payload: dict = Body(...)):
+    print(payload)
+    return {"message": "successfully created posts"}
